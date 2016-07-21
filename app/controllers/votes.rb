b210e74047a4ec3962b3ee17e3ff36a2f5637f0e
Wolfgang A Criollo <email": null>
2016-07-21 14:50:16 -0400

    add fucntion to prevent duplicate votes

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,57 +1,52 @@
 post '/questions/votes/:id' do
 
-if logged_in?
 
-  if params[:uservote] == "upvote"
-    @vote = Vote.new(value: 1)
-    current_question.votes << @vote
-  elsif params[:uservote] == "downvote"
-    @vote = Vote.new(value: -1)
-    current_question.votes << @vote
-  end
 
-  if @vote.save
-    redirect "/questions/#{params[:id]}"
+  if logged_in? && !has_voted?(current_user.id)
+    if params[:uservote] == "upvote"
+      @vote = Vote.new(value: 1, user_id: current_user.id)
+      current_question.votes << @vote
+    elsif params[:uservote] == "downvote"
+      @vote = Vote.new(value: -1)
+      current_question.votes << @vote
+    end
+
+    if @vote.save
+      redirect "/questions/#{params[:id]}"
+    else
+      redirect "/questions/#{params[:id]}"
+    end
+
   else
+    @error = "You must login to vote"
     redirect "/questions/#{params[:id]}"
   end
-
-else
-  @error = "You must login to vote"
-  redirect "/questions/#{params[:id]}"
-end
 
 end
 
 
 post '/answers/votes/:id' do
 
-if logged_in?
-  if params[:uservote] == "upvote"
-    @vote = Vote.new(value: 1)
+  if logged_in? && !has_voted?(current_user.id)
+    if params[:uservote] == "upvote"
+      @vote = Vote.new(value: 1)
+      Answer.find(params[:id]).votes << @vote
+    elsif params[:uservote] == "downvote"
+      @vote = Vote.new(value: -1)
+      Answer.find(params[:id]).votes << @vote
+    end
 
-    Answer.find(params[:id]).votes << @vote
-    # current_answer.votes << @vote
+    answer_question_id = Answer.find(params[:id]).question_id
+    if @vote.save
+      redirect "/questions/#{answer_question_id}"
+    else
+      redirect "/questions/#{answer_question_id}"
+    end
 
-  elsif params[:uservote] == "downvote"
-    @vote = Vote.new(value: -1)
-  Answer.find(params[:id]).votes << @vote
-    # current_answer.votes << @vote
-  end
-
-  answer_question_id = Answer.find(params[:id]).question_id
-
-
-  if @vote.save
-    redirect "/questions/#{answer_question_id}"
   else
+    @error = "You must login to vote"
     redirect "/questions/#{answer_question_id}"
   end
-
-else
-  @error = "You must login to vote"
-  redirect "/questions/#{qid}"
-end
 
 end
 
