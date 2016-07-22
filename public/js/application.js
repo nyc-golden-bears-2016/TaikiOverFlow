534ac9e065d674bc829356de36a4526a20f626d1
Wolfgang Criollo <wolfplisken@gmail.com>
2016-07-21 22:40:22 -0400

    add hide and show comments form box

--- public/js/application.js
+++ public/js/application.js
@@ -1,19 +1,32 @@
-$(document).ready(function() {
+$(function() {
 
 
-  $('#comment-form').submit(function(event){
-      event.preventDefault();
+// comment boxes hide and show
+var $comment_form = $('.col-group #comment-form');
+$comment_form.each(function(){
+  var $comment_form = $(this).children("#comment_form")
+  var $comment_button = $(this).children(".comment-btn");
+  $(this).after('<span class="comment-btn">Leave a comment</span>');
+  $(this).hide();
 
-    $.ajax({
-        url: $(this).attr('action'),
-        method: $(this).attr('method')
-    }).done(function(response))
+});
 
+$comment_button = $(".comment-btn")
+$comment_button.on('click',function(){
+  $(this).prev($comment_form).show();
+  $(this).hide();
+})
 
-
-
-
-    })
+  // $('#comment-form').submit(function(event){
+  //     event.preventDefault();
+  //
+  //   $.ajax({
+  //       url: $(this).attr('action'),
+  //       method: $(this).attr('method')
+  //   }).done(function(response))
+  //
+  //
+  //   })
 
 
 });
