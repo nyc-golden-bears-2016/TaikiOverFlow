eed13929a57a563e6ede7afcefa5b41e1d19ecd8
Taiki Kosugi <taikikosugi@gmail.com>
2016-07-22 10:05:54 -0400

    add js file

--- public/js/application.js
+++ public/js/application.js
@@ -1,38 +1,17 @@
 $(document).ready(function() {
 	$('#question_arrow-up').click(function(event){
 		event.preventDefault();
-
-  var question_id = $(this).prev().val();
-
+	  var question_id = $(this).prev().val();
 		var request = $.ajax({
-      method: 'POST',
-      url: '/questions/votes/'+question_id,
-      data: {uservote: "upvote"}
-    });//.done(function(response){
-      // $('#die-container').html("<div class=\"die\"> <span class=\"roll\">"+response+"</span> </div>")
-      //$('#question_votes').html(response);
-      //      });
-      request.done(function(response){
-      	// alert('request done');
+	    method: 'POST',
+	    url: '/questions/votes/'+question_id,
+	    data: {uservote: "upvote"}
+	  });
+		request.done(function(response){
 			$('#question_votes').html(response);
-
-      });
-
+		});
 	});
 
 
-  $('#comment-form').submit(function(event){
-      event.preventDefault();
-
-    $.ajax({
-        url: $(this).attr('action'),
-        method: $(this).attr('method')
-    }).done(function(response))
-
-
-
-
-
-    })
 
 });
